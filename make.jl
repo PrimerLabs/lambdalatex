44f67b480530930ccd07bdc2a00d235db70484e3
wzard <sid.dharth.soka@gmail.com>
2018-07-03 22:18:06 +0530

    Changed the julia make file

--- make.jl
+++ make.jl
@@ -4,7 +4,7 @@ using JSON
 using AWSCore
 using AWSS3
 # using AWSLambda: create_lambda, update_lambda, lambda_configuration,
-                 invoke_lambda
+#                 invoke_lambda
 using InfoZIP
 
 function all()
